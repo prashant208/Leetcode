10c40fb9f0c26891bc23822df15145be7bb61e3b
prashant208 <vb05446@gmail.com>
2024-03-17 12:00:16 +0530

    Implement Rand10() using Rand7()

---
+++ 470. Implement Rand10() Using Rand7()/main.java
@@ -0,0 +1,16 @@
+/**
+ * The rand7() API is already defined in the parent class SolBase.
+ * public int rand7();
+ * 
+ * @return a random integer in the range 1 to 7
+ */
+ 
+ class main extends SolBase {
+    int x = 1;
+
+    public int rand10() {
+        if (x >= 11)
+            x = 1;
+        return x++;
+    }
+}
