6372138386fa65210ec9b0547e897b50c13e9b38
prashant208 <vb05446@gmail.com>
2024-04-05 07:33:48 +0530

    Maximum nesting depth of the Paranthesis

---
+++ 1614. Maximum Nesting Depth of the Parentheses/main.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(String s) {
+        int maxDepth = 0;
+        int currentDepth = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (c == '('){
+                currentDepth++;
+                maxDepth = Math.max(maxDepth, currentDepth);
+            }else if (c == ')'){
+                currentDepth--;
+            }
+        }
+        return maxDepth;
+    }
+}
