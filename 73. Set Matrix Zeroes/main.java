32357f624e03bf219c18526158a8395f1c212433
prashant208 <vb05446@gmail.com>
2024-04-07 17:58:28 +0530

    Set Matrix Zeroes

---
+++ 73. Set Matrix Zeroes/main.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[] row = new int[n]; // row array
+        int[] col = new int[m]; // col array
+
+        // Traverse the matrix:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    // mark ith index of row wih 1:
+                    row[i] = 1;
+
+                    // mark jth index of col wih 1:
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Finally, mark all (i, j) as 0
+        // if row[i] or col[j] is marked with 1.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
