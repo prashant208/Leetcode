244030ea75a8a9cb7dd79edd22d4b6ae048c2c3d
prashant208 <vb05446@gmail.com>
2024-04-10 07:13:52 +0530

    Pascal's Triangle

---
+++ 118. Pascal's Triangle/main.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int nCr(int n, int r) {
+        long res = 1;
+        // calculating nCr:
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return (int) res;
+    }
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int row=1 ; row <= n; row++){
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(int col = 1; col <= row; col++){
+                temp.add(nCr(row-1, col-1));
+            }
+            ans.add(temp);
+        }
+        return ans;
+    }
+}
